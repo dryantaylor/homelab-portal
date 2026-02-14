10761152daed000b7b97c5ba35f186b15ca9ff2b
Dianna Ryan-Taylor <72103204+dryantaylor@users.noreply.github.com>
2026-02-14 00:28:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import yaml
+import argparse, sys
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,4 +10,23 @@ def index():
         return render_template('index.html', yaml= yaml.safe_load(server_content))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    parser=argparse.ArgumentParser()
+
+    parser.add_argument("--port", help="Port to run on, default 5000", default=5000)
+    parser.add_argument("--debug", help="enable debug logs, default False",default=False)
+
+    args=parser.parse_args()
+    try:
+        port = int(args.port)
+    except ValueError:
+        print("Invalid port number. Please provide an integer value for the port.")
+        sys.exit(1)
+    
+    try:
+        debug = bool(args.debug)
+    except ValueError:
+        print("Invalid debug value. Please provide a boolean value (True/False) for debug.")
+        sys.exit(1)
+
+    app.run(host='0.0.0.0', port=args.get(), debug=True)
+
